7e1e9cf2fb82ae5ed7a6382eae5867c8038af041
Nayanaa123 <nayana162006@gmail.com>
2024-04-25 12:02:28 -0700

    package

---
+++ 1.py
@@ -0,0 +1,4 @@
+from pack import *
+sum(10,20)
+sub(10,20)
+mul(10,20)
---
+++ pack.py
@@ -0,0 +1,11 @@
+def sum(a,b):
+    c=a+b
+    print("sum:",c)
+def sub(a,b):
+    d=a-b
+    print("subrate:",d)
+def mul(a,b):
+    e=a*b 
+    print("multiple:",e)
+
+     
