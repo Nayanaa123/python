c60888d84565d714fec82baea75dc834311e4d71
Nayanaa123 <nayana162006@gmail.com>
2024-05-03 09:40:01 -0700

    file add

---
+++ a.py
@@ -0,0 +1,10 @@
+#a=open("first.py","r")
+#content=a.read()
+#print(content)
+#a.close()
+#b=open("b.py","w")
+b=open("b.py","a")
+b.write("hiiii")
+b.close()
+
+
--- new.py
+++ new.py
@@ -2,9 +2,13 @@ def data(a,b,c,d):
     global total
     global avg 
     global per 
+    global mul
+    global div
     total=a+b+c+d 
     avg=total/4
     per=total*100/400
+    mul=a*b*c*d
+    div=a/b/c/d
 def total():
         print("Total :",total)
 def avg():
@@ -19,9 +23,13 @@ def grade():
        elif per>=50:
              print("c",per)
        else:
-              print("d",per)       
-              
-                          
+              print("d",per)
+def mul():
+       print("multiple",mul) 
+def div():
+       print("divide",div)                   
+
+                                
 
 
         
