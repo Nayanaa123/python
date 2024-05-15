626e9dcdc4366695abc571158353fb4988991241
Nayanaa123 <nayana162006@gmail.com>
2024-05-15 11:16:50 -0700

    database

--- inheridance.py
+++ inheridance.py
@@ -1,13 +1,19 @@
 class first:
     def one(self):
         print("its first program")
-class second():
+class second(first):
         def two(self):
              print("its second program")
-class third(first,second):
+class fourth:
+     def four(self):
+          print("its fourth program")
+class third(second,fourth):
      def three(self):
           print("its third program")
 x=third()
 x.one()
 x.two()
 x.three()
+x.four()
+
+
